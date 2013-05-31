fee68723de39fa77441294162416934258bd2e58
Mark Sadegi <mark.sadegi@gmail.com>
2013-05-31 16:02:30 -0500

    added tests

--- msadegi/spec/find_largest_prime_spec.rb
+++ msadegi/spec/find_largest_prime_spec.rb
@@ -2,18 +2,18 @@ require "spec_helper"
 
 describe LargestPrimeFactor do
 	it "should accept only a number" do
-		@bad_prime = LargestPrimeFactor.do_it_to_it("TEST")
+		@bad_prime = LargestPrimeFactor.look_on_the_internet_for_someone_elses_brilliant_solution("TEST")
 		@bad_prime.find_largest_prime_factor.should == "Input must be an integer"
 	end
 
 	it "should return the number if less than 4" do
-		@prime = LargestPrimeFactor.do_it_to_it(3)
+		@prime = LargestPrimeFactor.look_on_the_internet_for_someone_elses_brilliant_solution(3)
 		@prime.find_largest_prime_factor.should == 3
 	end
 
 	it "should find the largest prime number" do
-		@number = LargestPrimeFactor.do_it_to_it(39)
-		@number.find_largest_prime_factor.should == 13
+		@number = LargestPrimeFactor.look_on_the_internet_for_someone_elses_brilliant_solution(151)
+		@number.find_largest_prime_factor.should == 151
 	end
 
 end
